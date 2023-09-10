bc218f8e188b1147fa4aca77d1a4f51b74c53ea9
Harold-Chan <cch.harold@gmail.com>
2023-09-09 20:41:30 -0400

    budget not lower than spending

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -4,13 +4,21 @@ import { AppContext } from '../context/AppContext';
 const Budget = () => {
     const { budget } = useContext(AppContext);
     const [newBudget, setNewBudget] = useState(budget);
+    const { expenses } = useContext(AppContext);
+    const totalExpenses = expenses.reduce((total, item) => {
+        return (total += item.cost);
+    }, 0);
     const handleBudgetChange = (event) => {
         const value = event.target.value;
 
         if (value > 20000) {
             window.alert('Budget cannot exceed £20,000');
             setNewBudget(20000); // Set it to the maximum allowed value
-        } else {
+        } else if (value < totalExpenses) {
+            window.alert('You cannot reduce the budget value lower than the spending');
+            setNewBudget(totalExpenses); // Set it to the spending
+        } else
+        {
             setNewBudget(value);
         }
     };
