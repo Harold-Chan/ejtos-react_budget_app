7107ba5a9ab4deecd40aac9cffd08b7f5a56babf
Harold-Chan <cch.harold@gmail.com>
2023-09-10 16:52:03 -0400

    change currency prefix trial

--- src/components/Currency.js
+++ src/components/Currency.js
@@ -1,22 +1,34 @@
 import React from 'react';
 
-const Currency = () => {
+const Currency = (props) => {
+    const { selectedCurrency, handleCurrencyChange } = props;
+
     const optionStyle = {
         backgroundColor: 'black',
         color: 'white',
     };
 
-return (
-    <div className='alert alert-warning'>
-        <label for="currency">Currency: </label>
-        <select id="currency" style={optionStyle}>
-            <option value="Dollar" name="dollar">$ Dollar</option>
-            <option value="Pound" name="pound" selected>£ Pound</option>
-            <option value="Euro" name="euro">€ Euro</option>
-            <option value="Ruppee" name="ruppee">₹ Ruppee</option>
-        </select>
-    </div>
-);
+    // Event handler for the currency dropdown change event
+    const handleChange = (event) => {
+        handleCurrencyChange(event.target.value);
+    };
+
+    return (
+        <div className='alert alert-warning'>
+            <label htmlFor="currency">Currency: </label>
+            <select
+                id="currency"
+                style={optionStyle}
+                onChange={handleChange}
+                value={selectedCurrency}
+            >
+                <option value="Dollar" name="dollar">$ Dollar</option>
+                <option value="Pound" name="pound">£ Pound</option>
+                <option value="Euro" name="euro">€ Euro</option>
+                <option value="Ruppee" name="ruppee">₹ Ruppee</option>
+            </select>
+        </div>
+    );
 };
 
 export default Currency;
