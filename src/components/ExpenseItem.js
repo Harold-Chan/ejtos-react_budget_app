ed762c1eff7064ed555e36f141a481a025b29a72
Harold-Chan <cch.harold@gmail.com>
2023-09-11 18:40:50 -0400

    the minus button

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -5,12 +5,16 @@ import { AppContext } from '../context/AppContext';
 const ExpenseItem = (props) => {
     const {selectedCurrency} = props; // Receive the selected currency as a prop
     const optionStyle1 = {
-        // width: '10px',
-        // height: '10px',
         color: 'white',
-        fontSize: '120%',
         fontWeight: 'bold',
         backgroundColor: 'forestgreen',
+        borderRadius: '50%',
+        borderWidth: '0',
+    };
+    const optionStyle2 = {
+        color: 'white',
+        fontWeight: 'bold',
+        backgroundColor: 'red',
         borderRadius: '50%',
         borderWidth: '0',
     };
@@ -32,15 +36,27 @@ const ExpenseItem = (props) => {
         dispatch({
             type: 'ADD_EXPENSE',
             payload: expense
-        });
+        }); 
+    }
 
+    const decreaseAllocation = (name) => {
+        const expense = {
+            name: name,
+            cost: 10,
+        };
+
+        dispatch({
+            type: 'RED_EXPENSE',
+            payload: expense
+        }); 
     }
 
     return (
         <tr>
         <td>{props.name}</td>
         <td>{selectedCurrency}{props.cost}</td>
-        <td ><button style={optionStyle1} onClick={event=> increaseAllocation(props.name)}> + </button></td>
+        <td><button style={optionStyle1} onClick={event=> increaseAllocation(props.name)}> + </button></td>
+        <td><button style={optionStyle2} onClick={event=> decreaseAllocation(props.name)}> - </button></td>
         <td><TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete></td>
         </tr>
     );
